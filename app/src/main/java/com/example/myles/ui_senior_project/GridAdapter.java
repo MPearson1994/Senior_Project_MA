0908e6157fcdbefe838de37a28c4011ca06a4e54
MPearson1994 <MPearso2@my.tnstate.edu>
2017-09-20 17:55:25 -0500

    September 20th, 2017
    I added some code to the Mobile App to get a Grid View for the Icons I found

---
+++ app/src/main/java/com/example/myles/ui_senior_project/GridAdapter.java
@@ -0,0 +1,61 @@
+package com.example.myles.ui_senior_project;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Myles.lab on 9/20/2017.
+ */
+
+public class GridAdapter extends BaseAdapter {
+
+    Context context;
+
+    private final String[] values;
+    private final  int [] images;
+
+    View view;
+    LayoutInflater layoutInflater;
+
+    public GridAdapter(Context context, String[] values, int[] images) {
+        this.context = context;
+        this.values = values;
+        this.images = images;
+    }
+
+
+    @Override
+    public int getCount() {
+        return values.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        if(convertView == null){
+            view = new View(context);
+            view = layoutInflater.inflate(R.layout.fragment_home, null);
+            ImageView imageView = (ImageView)view.findViewById(R.id.imageView);
+            TextView textView = (TextView) view.findViewById(R.id.textView);
+            imageView.setImageResource(images[i]);
+            textView.setText(values[i]);
+        }
+        return view;
+    }
+}
--- app/src/main/java/com/example/myles/ui_senior_project/Home.java
+++ app/src/main/java/com/example/myles/ui_senior_project/Home.java
@@ -7,14 +7,49 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class Home extends Fragment {
+    GridView gridView;
+
+    private String[] values = {
+            "Closet",
+            "Weather",
+            "Calendar",
+            "Camera",
+            "Favorites",
+            "Settings"
+    };
+
+    // references to our images
+    private int[] images = {
+            R.drawable.closet,
+            R.drawable.weather,
+            R.drawable.calendar,
+            R.drawable.camera,
+            R.drawable.favorites,
+            R.drawable.settings
+    };
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        gridView = (GridView) rootView.findViewById(R.id.GridMenu_Home);
+        GridAdapter gridAdapter =  new GridAdapter(this.getActivity(), values, images);
+    gridView.setAdapter(gridAdapter);
+        return rootView;
     }
+
+
+
+
+
 
 }
